400336de8ca5207c761573fd8b20ced88357f294
Steve Puddick <spuddick@liberal.ca>
2017-01-17 16:50:01 -0500

    change: remove doc files

--- test/templates/inlinephp_expected.php
+++
@@ -1,8 +0,0 @@
-<div id="access">
-    <div class="skip-link screen-reader-text">
-        <a <?php atts(array('href' => "#content", 'title' => 'Skip to content')); ?>>
-        	<?php echo 'Skip to content'; ?>
-        </a>
-    </div>
-    <?php echo 'Hello, there!'; ?>
-</div>
